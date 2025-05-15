7c90ffa812e2a36e6cb468d9c7e2b85e37c59484
ESTEBAN RINALDY <erinaldy@unicesar.edu.co>
2025-05-15 17:08:40 -0500

    Cambiosss

--- Entity/Producto.cs
+++ Entity/Producto.cs
@@ -4,8 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Bll
+namespace Entity
 {
-    public class Class1
-   
+    internal class Producto
+    {
+    }
 }
