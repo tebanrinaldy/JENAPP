ee76fe3ff264485b684808f5ba05ff4fb5d19004
JOSE ZABALETA <jmzabaleta@unicesar.edu.co>
2025-05-22 02:07:10 -0500

    mmn

--- Visual/FrmCategorias.Designer.cs
+++ Visual/FrmCategorias.Designer.cs
@@ -44,7 +44,7 @@
             this.BarraTitulo.Dock = System.Windows.Forms.DockStyle.Top;
             this.BarraTitulo.Location = new System.Drawing.Point(0, 0);
             this.BarraTitulo.Name = "BarraTitulo";
-            this.BarraTitulo.Size = new System.Drawing.Size(1282, 35);
+            this.BarraTitulo.Size = new System.Drawing.Size(1080, 35);
             this.BarraTitulo.TabIndex = 25;
             // 
             // BtnGuardarProducto
@@ -52,7 +52,7 @@
             this.BtnGuardarProducto.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
             this.BtnGuardarProducto.FlatAppearance.BorderSize = 0;
             this.BtnGuardarProducto.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.BtnGuardarProducto.Location = new System.Drawing.Point(482, 304);
+            this.BtnGuardarProducto.Location = new System.Drawing.Point(388, 308);
             this.BtnGuardarProducto.Name = "BtnGuardarProducto";
             this.BtnGuardarProducto.Size = new System.Drawing.Size(204, 35);
             this.BtnGuardarProducto.TabIndex = 33;
@@ -66,7 +66,7 @@
             this.label3.BackColor = System.Drawing.Color.Transparent;
             this.label3.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.ForeColor = System.Drawing.Color.White;
-            this.label3.Location = new System.Drawing.Point(342, 236);
+            this.label3.Location = new System.Drawing.Point(225, 237);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(93, 23);
             this.label3.TabIndex = 30;
@@ -74,7 +74,7 @@
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(468, 237);
+            this.textBox2.Location = new System.Drawing.Point(364, 237);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(259, 22);
             this.textBox2.TabIndex = 29;
@@ -85,7 +85,7 @@
             this.label2.BackColor = System.Drawing.Color.Transparent;
             this.label2.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.Color.White;
-            this.label2.Location = new System.Drawing.Point(373, 190);
+            this.label2.Location = new System.Drawing.Point(225, 190);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(30, 23);
             this.label2.TabIndex = 28;
@@ -93,7 +93,7 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(468, 193);
+            this.textBox1.Location = new System.Drawing.Point(364, 193);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(259, 22);
             this.textBox1.TabIndex = 27;
@@ -104,7 +104,7 @@
             this.label1.BackColor = System.Drawing.Color.Transparent;
             this.label1.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.White;
-            this.label1.Location = new System.Drawing.Point(529, 147);
+            this.label1.Location = new System.Drawing.Point(424, 110);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(136, 23);
             this.label1.TabIndex = 26;
@@ -114,9 +114,9 @@
             // 
             this.listBox1.FormattingEnabled = true;
             this.listBox1.ItemHeight = 16;
-            this.listBox1.Location = new System.Drawing.Point(883, 172);
+            this.listBox1.Location = new System.Drawing.Point(703, 111);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(314, 196);
+            this.listBox1.Size = new System.Drawing.Size(314, 260);
             this.listBox1.TabIndex = 34;
             // 
             // FrmCategorias
@@ -124,7 +124,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(49)))), ((int)(((byte)(66)))), ((int)(((byte)(82)))));
-            this.ClientSize = new System.Drawing.Size(1282, 603);
+            this.ClientSize = new System.Drawing.Size(1080, 615);
             this.Controls.Add(this.listBox1);
             this.Controls.Add(this.BtnGuardarProducto);
             this.Controls.Add(this.label3);
@@ -135,6 +135,7 @@
             this.Controls.Add(this.BarraTitulo);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "FrmCategorias";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "FrmCategorias";
             this.ResumeLayout(false);
             this.PerformLayout();
